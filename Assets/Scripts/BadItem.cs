2d1ae487b07c3e5fb3e7daf7920de971287e43d3
Matthew Alex <doson2435@gmail.com>
2025-10-27 18:47:22 +0700

    add baditem

---
+++ Assets/Scripts/BadItem.cs
@@ -0,0 +1,34 @@
+using UnityEngine;
+
+public class BadItem : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public GameObject badItemPrefab;
+    public float speed = 5f;
+
+    void Update()
+    {
+        transform.Translate(Vector3.down * speed * Time.deltaTime); //tạo chuyển động theo phương thẳng đứng hướng xuống với tốc độ trên theo thời gian
+    }
+
+    //biến nhận thông tin về game object đang va chạm được đặt tên là other
+    void OnTriggerEnter2D(Collider2D other) //other chính là thông tin của bất kỳ collider nào va chạm với collider này
+    {
+        // thiết lập điều kiện kiểm tra thông tin của OTHER
+        if (other.gameObject.CompareTag("Player"))
+        // nếu, phương thức so sánh gameobject tag của other với nhãn "Player" là đúng
+        { // thì
+            ScoreManager.AddScore(-1);
+            AudioSource audioSource = other.GetComponent<AudioSource>();
+            //play âm thanh từ component đó
+            audioSource.Play();
+            Destroy(gameObject); //xóa gameObject đang gắn collider này. (Không phải là other)
+                                 //nghĩa là xóa viên ngọc này, không phải xóa người chơi đang va chạm
+        }
+        else if (other.gameObject.CompareTag("Ground")) // còn không thì, nếu là mặt đất,
+        { // thì
+            Destroy(gameObject); //xóa gameObject đang gắn collider này. (Không phải là other)
+                                 //nghĩa là xóa viên ngọc này, không phải xóa mặt đất
+        }
+    }
+}
